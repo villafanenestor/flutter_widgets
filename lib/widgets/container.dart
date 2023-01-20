dfec6de98c69dd719ab01efc355bc53b0623a697
Nestor Villafane <mass.nestor@gmail.com>
2023-01-19 21:52:38 -0500

    se agrega boxDecoratio

--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -3,18 +3,22 @@ import 'package:flutter/material.dart';
 class MyCustomContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Container(
+      color: Colors.white,
       child: DefaultTextStyle(
         style: const TextStyle(),
         child: Center(
           child: Container(
-            width: 250,
-            height: 250,
-            color: Colors.indigo[300],
-            margin: EdgeInsets.only(left: 30),
+            width: 200,
+            height: 200,
             alignment: Alignment.center,
-            child: const Text(
-                "Hola como estas lorem loremlorem loremlorem loremlorem loremlorem lorem"),
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                  color: Colors.red.withOpacity(0.95),
+                  blurRadius: 40,
+                  offset: Offset(50, 30)),
+            ], color: Colors.indigo[300], shape: BoxShape.circle),
+            child: const Text("Hola"),
           ),
         ),
       ),
